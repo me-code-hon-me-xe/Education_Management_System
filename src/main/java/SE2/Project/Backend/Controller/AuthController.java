d39e7bfc3bc1fe304749b82bf1dbceaf1260e01c
Lê Tất Thành <leethanh210703@gmail.com>
2024-04-17 14:00:49 +0700

    layout init

--- src/main/java/SE2/Project/Backend/Controller/AuthController.java
+++ src/main/java/SE2/Project/Backend/Controller/AuthController.java
@@ -1,15 +1,19 @@
 package SE2.Project.Backend.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AuthController {
     @GetMapping("/")
-    public String login() {
+    public String login(Model model) {
+
         return "LoginScreen";
     }
+
     @GetMapping("/home")
     public String home(){
         return "teacher-grade-management";
--- src/main/java/SE2/Project/Backend/Service/SecurityCfg.java
+++ src/main/java/SE2/Project/Backend/Service/SecurityCfg.java
@@ -1,17 +1,18 @@
-//package SE2.Project.Backend.Service;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//public class SecurityCfg {
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .authorizeHttpRequests(
-//                        req->req
-////                                .requestMatchers("/login").permitAll()
-////                                .anyRequest().permitAll()
-//                ).build();
-//    }
-//}
+package SE2.Project.Backend.Service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+public class SecurityCfg {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                        req->req
+                                .requestMatchers("/").permitAll()
+                                .anyRequest().authenticated()
+                ).build();
+    }
+}
