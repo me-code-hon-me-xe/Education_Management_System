5ffa983117b0a8facd312976b27bad1461be46b9
Lê Tất Thành <leethanh210703@gmail.com>
2024-04-17 13:39:07 +0700

    Le thanh (#5)
    * sidebar and header
    
    * teacher grade management screen init (can't use layout for some reason?)
    
    * layout init
    
    * add login screen
    
    * Create README.md
    
    * layout init
    
    * Create lmao.txt
    
    * add login screen
    
    * Delete src/main/resources/template  directory
    
    * Update LoginScreen.html: fix alignment of forgot ur password
    
    * Authentication init
    
    * Authentication init
    
    ---------
    
    Co-authored-by: Wat <chiptp1912@gmail.com>
    Co-authored-by: iamduong123 <93998682+iamduong123@users.noreply.github.com>

---
+++ src/main/java/SE2/Project/Backend/Service/SecurityCfg.java
@@ -0,0 +1,17 @@
+//package SE2.Project.Backend.Service;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//public class SecurityCfg {
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .authorizeHttpRequests(
+//                        req->req
+////                                .requestMatchers("/login").permitAll()
+////                                .anyRequest().permitAll()
+//                ).build();
+//    }
+//}
