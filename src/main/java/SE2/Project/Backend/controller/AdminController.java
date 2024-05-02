55dd8abef3ffbd8c17f19ba2aef9b4c1b015fdda
Lê Tất Thành <leethanh210703@gmail.com>
2024-05-02 22:08:50 +0700

    init CRUD users for admin

--- src/main/java/SE2/Project/Backend/controller/AdminController.java
+++ src/main/java/SE2/Project/Backend/controller/AdminController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.expression.Strings;
 
 import java.util.List;
 
@@ -28,6 +30,8 @@ public class AdminController {
     @Autowired
     private TeacherRepository teacherRepository;
     @Autowired
+    private AccountantRepository accountantRepository;
+    @Autowired
     private CourseRepository courseRepository;
 
     // CRUD admin
@@ -37,14 +41,98 @@ public class AdminController {
         return "adminAdd";
     }
 
-    @RequestMapping("/insertAdmin")
-    public String insertAdmin(@Valid Admin admin, BindingResult result, Model model){
+    @RequestMapping(value = "/insertAdmin")
+    public String insertAdmin(@Valid Admin admin, BindingResult result, Model model) {
+
+        if(result.hasErrors()){
+            return "adminAdd";
+        } else if (isDuplicateEntry(admin.getUser().getUsername())) {
+            result.rejectValue("user.username", "duplicate.key", "Username already exists");
+            return "adminAdd";
+        }
         User user = admin.getUser();
         admin.setUser(user);
         userRepository.save(user);
         adminRepository.save(admin);
-        return "redirect:/admin/adminDetail/" + user.getId();
+
+        return "redirect:/admin/listAdmin";
     }
+
+    @GetMapping(value = "/listAdmin")
+    public String showAllAdmin(Model model, @RequestParam(name = "adminCode", required = false) Integer adminCode,
+                               @RequestParam(name = "showAll", required = false) String showAll){
+        Iterable<Admin> admins;
+        Admin admin;
+        if (showAll != null) {
+            admins = adminRepository.findAll();
+            model.addAttribute("admins", admins);
+            return "adminList";
+        }
+        if(adminCode != null){
+            admin = adminRepository.findByAdminCode(adminCode);
+            if(admin != null){
+                model.addAttribute("admins", admin);
+            } else {
+                model.addAttribute("notFoundMessage", "No teacher found with the provided admin code");
+            }
+        } else {
+            admins = adminRepository.findAll();
+            model.addAttribute("admins", admins);
+        }
+
+        return "adminList";
+    }
+
+    @GetMapping(value = "/adminDetail/{adminCode}")
+    public String showAdminDetail(@PathVariable Integer adminCode, Model model){
+        Admin admin = adminRepository.findByAdminCode(adminCode);
+        model.addAttribute("admin", admin);
+        return "adminDetail";
+    }
+
+    @GetMapping(value = "/updateAdmin/{adminCode}")
+    public String updateAdmin(@PathVariable Integer adminCode, Model model){
+        Admin admin = adminRepository.findByAdminCode(adminCode);
+        model.addAttribute("admin", admin);
+        return "adminUpdate";
+    }
+
+    @PostMapping(value = "/saveAdmin")
+    public String saveAdmin(@Valid Admin admin, BindingResult result){
+        if(result.hasErrors()){
+            return "adminUpdate";
+        }
+        User user = admin.getUser();
+        userRepository.save(user);
+        adminRepository.save(admin);
+        return "redirect:/admin/listAdmin";
+    }
+
+    @RequestMapping(value = "/deleteAdmin/{adminCode}")
+    public String deleteAdmin(@PathVariable Integer adminCode){
+        Admin admin = adminRepository.findByAdminCode(adminCode);
+        User user = admin.getUser();
+        adminRepository.delete(admin);
+        userRepository.delete(user);
+        return "redirect:/admin/listAdmin";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     // CRUD student
     @GetMapping("/addStudent")
@@ -56,21 +144,99 @@ public class AdminController {
     @RequestMapping(value = "/insertStudent")
     public String insertStudent(@Valid Student student, BindingResult result, Model model) {
 
+        if(result.hasErrors()){
+            return "studentAdd";
+        } else if (isDuplicateEntry(student.getUser().getUsername())) {
+            result.rejectValue("user.username", "duplicate.key", "Username already exists");
+            return "studentAdd";
+        }
         User user = student.getUser();
         student.setUser(user);
         userRepository.save(user);
         studentRepository.save(student);
 
-        return "redirect:/admin/studentDetail/" + user.getId();
-
+        return "redirect:/admin/listStudent";
     }
 
     @GetMapping(value = "/listStudent")
-    public String showAllStudents(Model model){
-        Iterable<Student> students = studentRepository.findAll();
-        model.addAttribute("students", students);
+    public String showAllStudents(Model model, @RequestParam(name = "studentCode", required = false) Integer studentCode,
+                                  @RequestParam(name = "showAll", required = false) String showAll){
+        Iterable<Student> students;
+        Student student;
+        if (showAll != null) {
+            students = studentRepository.findAll();
+            model.addAttribute("students", students);
+            return "studentList";
+        }
+        if(studentCode != null){
+            student = studentRepository.findByStudentCode(studentCode);
+            if(student != null){
+                model.addAttribute("students", student);
+            } else {
+                model.addAttribute("notFoundMessage", "No student found with the provided student code");
+            }
+        } else {
+            students = studentRepository.findAll();
+            model.addAttribute("students", students);
+        }
+
         return "studentList";
     }
+
+    @GetMapping(value = "/studentDetail/{studentCode}")
+    public String showStudentDetail(@PathVariable Integer studentCode, Model model){
+        Student student = studentRepository.findByStudentCode(studentCode);
+        System.out.println(student.getUser().getEmail());
+        model.addAttribute("student", student);
+        return "studentDetail";
+    }
+
+
+
+    @GetMapping(value = "/updateStudent/{studentCode}")
+    public String updateStudent(@PathVariable Integer studentCode, Model model){
+        Student student = studentRepository.findByStudentCode(studentCode);
+        model.addAttribute("student", student);
+        return "studentUpdate";
+    }
+
+
+    @PostMapping("/saveStudent")
+    public String saveStudent(@Valid @ModelAttribute("student") Student student, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "studentUpdate"; // Return to the update form if there are validation errors
+        }
+        User user = student.getUser();
+        userRepository.save(user);
+        // Save the student
+        studentRepository.save(student);
+
+        // Redirect to the student list page
+        return "redirect:/admin/listStudent";
+    }
+
+    @RequestMapping(value = "/deleteStudent/{studentCode}")
+    public String deleteStudent(@PathVariable Integer studentCode){
+        Student student = studentRepository.findByStudentCode(studentCode);
+        User user = student.getUser();
+        studentRepository.delete(student);
+        userRepository.delete(user);
+        return "redirect:/admin/listStudent";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     // CRUD teacher
@@ -82,15 +248,187 @@ public class AdminController {
 
     @RequestMapping(value = "/insertTeacher")
     public String insertTeacher(@Valid Teacher teacher, BindingResult result, Model model) {
+        if(result.hasErrors()){
+            return "teacherAdd";
+        } else if (isDuplicateEntry(teacher.getUser().getUsername())) {
+            result.rejectValue("user.username", "duplicate.key", "Username already exists");
+            return "teacherAdd";
+        }
 
         User user = teacher.getUser();
         teacher.setUser(user);
         userRepository.save(user);
         teacherRepository.save(teacher);
 
-        return "redirect:/admin/teacherDetail/" + user.getId();
-
+        return "redirect:/admin/listTeacher";
     }
+
+    private boolean isDuplicateEntry(String username) {
+        System.out.println(userRepository.existsByUsername(username));
+        return userRepository.existsByUsername(username);
+    }
+
+    @GetMapping(value = "/listTeacher")
+    public String showAllTeachers(Model model, @RequestParam(name = "teacherCode", required = false) Integer teacherCode,
+                                  @RequestParam(name = "showAll", required = false) String showAll){
+        Iterable<Teacher> teachers;
+        Teacher teacher;
+        if (showAll != null) {
+            teachers = teacherRepository.findAll();
+            model.addAttribute("teachers", teachers);
+            return "teacherList";
+        }
+        if(teacherCode != null){
+            teacher = teacherRepository.findByTeacherCode(teacherCode);
+            if(teacher != null){
+                model.addAttribute("teachers", teacher);
+            } else {
+                model.addAttribute("notFoundMessage", "No teacher found with the provided teacher id");
+            }
+        } else {
+            teachers = teacherRepository.findAll();
+            model.addAttribute("teachers", teachers);
+        }
+
+        return "teacherList";
+    }
+
+    @GetMapping(value = "/teacherDetail/{teacherCode}")
+    public String showTeacherDetail(@PathVariable Integer teacherCode, Model model){
+        Teacher teacher = teacherRepository.findByTeacherCode(teacherCode);
+        model.addAttribute("teacher", teacher);
+        return "teacherDetail";
+    }
+
+    @GetMapping(value = "/updateTeacher/{teacherCode}")
+    public String updateTeacher(@PathVariable Integer teacherCode, Model model){
+        Teacher teacher = teacherRepository.findByTeacherCode(teacherCode);
+        model.addAttribute("teacher", teacher);
+        return "teacherUpdate";
+    }
+
+    @PostMapping(value = "/saveTeacher")
+    public String saveTeacher(@Valid Teacher teacher, BindingResult result){
+        if(result.hasErrors()){
+            return "teacherUpdate";
+        }
+        User user = teacher.getUser();
+        userRepository.save(user);
+        teacherRepository.save(teacher);
+        return "redirect:/admin/listTeacher";
+    }
+
+    @RequestMapping(value = "/deleteTeacher/{teacherCode}")
+    public String deleteTeacher(@PathVariable Integer teacherCode){
+        Teacher teacher = teacherRepository.findByTeacherCode(teacherCode);
+        User user = teacher.getUser();
+        teacherRepository.delete(teacher);
+        userRepository.delete(user);
+        return "redirect:/admin/listTeacher";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // CRUD accountant
+    @GetMapping("/addAccountant")
+    public String addAccountant(Model model) {
+        model.addAttribute("accountant", new Accountant());
+        return "accountantAdd";
+    }
+    @RequestMapping(value = "/insertAccountant")
+    public String insertAccountant(@Valid Accountant accountant, BindingResult result, Model model) {
+        if(result.hasErrors()){
+            return "accountantAdd";
+        } else if (isDuplicateEntry(accountant.getUser().getUsername())) {
+            result.rejectValue("user.username", "duplicate.key", "Username already exists");
+            return "accountantAdd";
+        }
+
+        User user = accountant.getUser();
+        accountant.setUser(user);
+        userRepository.save(user);
+        accountantRepository.save(accountant);
+
+        return "redirect:/admin/listAccountant";
+    }
+    @GetMapping(value = "/listAccountant")
+    public String showAllAccountant(Model model, @RequestParam(name = "accountantCode", required = false) Integer accountantCode,
+                                    @RequestParam(name = "showAll", required = false) String showAll){
+        Iterable<Accountant> accountants;
+        Accountant accountant;
+        if (showAll != null) {
+            accountants = accountantRepository.findAll();
+            model.addAttribute("accountants", accountants);
+            return "accountantList";
+        }
+        if(accountantCode != null){
+            accountant = accountantRepository.findByAccountantCode(accountantCode);
+            if(accountant != null){
+                model.addAttribute("accountants", accountant);
+            } else {
+                model.addAttribute("notFoundMessage", "No teacher found with the provided teacher id");
+            }
+        } else {
+            accountants = accountantRepository.findAll();
+            model.addAttribute("accountants", accountants);
+        }
+
+        return "accountantList";
+    }
+    @GetMapping(value = "/accountantDetail/{accountantCode}")
+    public String showAccountantDetail(@PathVariable Integer accountantCode, Model model){
+        Accountant accountant = accountantRepository.findByAccountantCode(accountantCode);
+        model.addAttribute("accountant", accountant);
+        return "accountantDetail";
+    }
+    @GetMapping(value = "/updateAccountant/{accountantCode}")
+    public String updateAccountant(@PathVariable Integer accountantCode, Model model){
+        Accountant accountant = accountantRepository.findByAccountantCode(accountantCode);
+        model.addAttribute("accountant", accountant);
+        return "accountantUpdate";
+    }
+
+    @PostMapping(value = "/saveAccountant")
+    public String saveAccountant(@Valid Accountant accountant, BindingResult result){
+        if(result.hasErrors()){
+            return "accountantUpdate";
+        }
+        User user = accountant.getUser();
+        userRepository.save(user);
+        accountantRepository.save(accountant);
+        return "redirect:/admin/listAccountant";
+    }
+
+    @RequestMapping(value = "/deleteAccountant/{accountantCode}")
+    public String deleteAccountant(@PathVariable Integer accountantCode){
+        Accountant accountant = accountantRepository.findByAccountantCode(accountantCode);
+        User user = accountant.getUser();
+        accountantRepository.delete(accountant);
+        userRepository.delete(user);
+        return "redirect:/admin/listAccountant";
+    }
+
+
+
+
+
+
+
+
+
 
     @GetMapping("/addCourse")
     public String addCourse(Model model) {
--- src/main/java/SE2/Project/Backend/repository/StudentRepository.java
+++ src/main/java/SE2/Project/Backend/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package SE2.Project.Backend.repository;
 import SE2.Project.Backend.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.servlet.tags.form.SelectTag;
 
@@ -13,4 +14,15 @@ public interface StudentRepository extends JpaRepository <Student, Long>{
 //    boolean existsByUsername(String username);
 //    boolean existsByEmail(String email);
 //    boolean existsByStudentCode(String studentCode);
+
+    // Define a custom query method to find a Student by userID
+    @Query("SELECT s FROM Student s WHERE s.user.userID = :userID")
+    Student findByUserId(@Param("userID") Long userID);
+
+
+    @Query("SELECT s FROM Student s WHERE s.studentCode = :studentCode")
+    Student findByStudentCode(@Param("studentCode") Integer studentCode);
+
+    @Query("DELETE FROM Student s WHERE s.studentCode = :studentCode")
+    void deleteByStudentCode(@Param("studentCode") Integer studentCode);
 }
