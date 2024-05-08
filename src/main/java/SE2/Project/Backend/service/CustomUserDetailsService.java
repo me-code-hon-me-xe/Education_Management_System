2c14385675eb8bf6433f3fdaabe6c8f87b08af7c
iamduong123 <htduong.22003@gmail.com>
2024-05-08 23:21:18 +0700

    Add Login feature (not done)

--- src/main/java/SE2/Project/Backend/service/CustomUserDetailsService.java
+++ src/main/java/SE2/Project/Backend/service/CustomUserDetailsService.java
@@ -7,18 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
+import java.util.logging.Logger;
 public class CustomUserDetailsService implements UserDetailsService {
-
+    private static final Logger logger = Logger.getLogger(CustomUserDetailsService.class.getName());
     @Autowired
     private UserRepository userRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info("Loading user details for username: " + username);
         User user = userRepo.findByUsername(username);
         if (user == null) {
+            logger.info("Loading user details for username: " + username);
             throw new UsernameNotFoundException("User not found");
         }
+        logger.info("User details loaded successfully for username: " + username);
         return new CustomUserDetails(user);
     }
 
