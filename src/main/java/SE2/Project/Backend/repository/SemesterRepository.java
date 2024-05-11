c659937203b6fdb4d90395da61c277683f00f7b2
DangTienDat <dat.dang.it.hnu@gmail.com>
2024-05-11 17:57:30 +0700

    Update Project to new Repository

--- src/main/java/SE2/Project/Backend/repository/SemesterRepository.java
+++
@@ -1,9 +0,0 @@
-package SE2.Project.Backend.repository;
-
-import SE2.Project.Backend.model.Semester;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SemesterRepository extends JpaRepository<Semester, Long> {
-    Semester findBySemesterId(Long semesterId);
-    boolean existsByStartYearAndEndYearAndSemesterNum(Integer startYear, Integer endYear, Integer semesterNum);
-}
